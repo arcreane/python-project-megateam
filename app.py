618acf9a3f6ed9553dd8742c646ef67eca42cde1
auguste <auguste.nass@ipsa.fr>
2025-11-03 14:24:23 +0100

    Mise a jour intérface dans le fichier interface.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-from tkinter import *
 
-fenetre = Tk()
-fenetre.title("ATC")
-fenetre.configure(bg="black")
-
-fenetre.attributes("-fullscreen", True)
-
-
-fenetre.bind("<Escape>", lambda e: fenetre.attributes("-fullscreen", False))
-
-label = Label(fenetre, text="Hello World", bg="black", fg="white", font=("Arial", 24))
-label.pack(expand=True)
-
-fenetre.mainloop()
