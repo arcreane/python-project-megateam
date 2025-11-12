5bcbffc367e945cd95a917ceb3ff2a1dcbce90e2
auguste <auguste.nass@ipsa.fr>
2025-11-12 14:37:59 +0100

    avion

---
+++ avion.py
@@ -0,0 +1,9 @@
+
+class Avion:
+    callsign: str = ""
+    y_km: float = 0.0
+    x_km: float = 0.0
+    altitude: float = 0.0
+    vitesse: float = 0.0
+    cap: float = 0.0
+    fuel: float = 0.0
