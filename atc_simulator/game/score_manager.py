dac8b6ebd788d54924af0fbee944d8c51bd56853
mathieu <matcougou@gmail.com>
2025-11-22 09:21:52 +0100

    def des fonctions qui définissent les points

--- atc_simulator/game/score_manager.py
+++ atc_simulator/game/score_manager.py
@@ -4,3 +4,16 @@ class ScoreManager:
 
     def __init__(self):
         self.score_actuel = 0
+
+
+
+    def reset_score(self):
+        self.score_actuel = 0
+
+
+
+    def evenement_jeu(self, type_evenement):
+        if type_evenement == "atterrissage":
+            self.score_actuel += self.SCORE_ATTERRISSAGE
+        elif type_evenement == "collision":
+            self.score_actuel += self.PENALITE_COLLISION
